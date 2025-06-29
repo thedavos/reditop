181888190d516f0781dcf5d8b4f7e790d12bf5ad
davidvargas <davidvargas.d45@gmail.com>
2025-06-29 01:43:26 -0500

    Integrate Tailwind CSS, update base styles, remove example views, and add new dependencies.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia, PiniaVuePlugin } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "tailwindcss/preflight.css";
 import "./assets/main.css";
 
 Vue.use(PiniaVuePlugin);
